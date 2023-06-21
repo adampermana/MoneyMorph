10b0e555a0ba4ebe691f48daac5f39d13846c556
Adam Permana <adampermana111@gmail.com>
2023-06-21 09:57:18 -0700

    Initial Commit

---
+++ MoneyMorph/MoneyMorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMorphApp.swift
+//  MoneyMorph
+//
+//  Created by Adam Permana on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
